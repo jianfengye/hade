3d390aba8df70d08fa9dbd3f2c90bd84f36e5a21
yejianfeng <jianfengye110@gmail.com>
2020-03-03 23:19:12 +0800

    update coverate

--- framework/provider/gorm/service.go
+++ framework/provider/gorm/service.go
@@ -6,9 +6,9 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 
-	// _ "github.com/jinzhu/gorm/dialects/mssql"
-	// _ "github.com/jinzhu/gorm/dialects/postgres"
-	// _ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/mssql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pkg/errors"
 )
 
